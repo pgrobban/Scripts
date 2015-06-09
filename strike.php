78a096fa02d32f52c3c75a3848541543657eb88b
Robert Sebescen <robert.sebescen@crossroads.se>
2015-06-09 10:27:40 +0200

    error handling in strike.php

--- strike.php
+++ strike.php
@@ -7,13 +7,14 @@ date_default_timezone_set("Europe/Stockholm");
 $swedish_day_names = array("Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag");
 $swedish_day_today = $swedish_day_names[date('N')-1]; // start array at 0
 echo "** Idag är det ".$swedish_day_today." ** \n";
+echo "Laddar meny...\n\n";
 
 $doc = new DOMDocument(); 
 //$doc->loadHTML(file_get_contents('lunchmeny.html')); // debug
 $doc->loadHTML(file_get_contents('http://www.strikebowlinggoteborg.se/restaurang/lunchmeny/'));
 $xpath = new DOMXpath($doc);
 
-// förväntad html-struktur:
+// exempel på förväntad html-struktur:
 /*
 <p><span style="text-decoration: underline;">Måndag</span></p>
 <ul>
@@ -23,6 +24,14 @@ $xpath = new DOMXpath($doc);
 </ul>
 */
 $elements = $xpath->query("//ul[preceding::p[1]/span[.='".$swedish_day_today."']]");
-foreach ($elements as $element) {
-    echo $element->nodeValue, PHP_EOL;
+if ($elements->length === 0)
+{
+	echo "Ingen mat idag :(\n";
+}
+else
+{
+	foreach ($elements as $element) 
+	{
+	    echo $element->nodeValue, PHP_EOL;
+	}
 }
